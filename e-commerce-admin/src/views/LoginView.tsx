813c29af7546a28f5f4dbb029870c105525a6104
jiasenwqbr <jasonjiasen@gmail.com>
2023-12-06 16:42:21 +0800

    Login page

--- e-commerce-admin/src/views/LoginView.tsx
+++ e-commerce-admin/src/views/LoginView.tsx
@@ -1,7 +1,63 @@
-function LoginView() {
-    return ( <div>
-        <h1>登录</h1>
-    </div> );
-}
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
 
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const LoginView: React.FC = () => {
+  return (
+    <div className="login-box">
+      <h2>
+        <span className="primary">KTT</span>Login
+      </h2>
+      <Form
+        name="basic"
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input placeholder="username" />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password placeholder="password" />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 export default LoginView;
